03ad1a38a2824880f06e7c56ced715902beb466f
Mukhopadhyay <praneshmukherjee7@gmail.com>
2021-09-29 03:23:59 +0530

    read_df method & tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,15 +5,18 @@ try:
 except ModuleNotFoundError:
     import tests.context
 
-# Third-party imports
+# Built-in modules
+import os
+
+# Third-party modules
 import pytest
 
-# RestDF imports
+# RestDF modules
 from restdf.utils import io
 from restdf.utils import exceptions
 
 @pytest.fixture
-def paths():
+def extension_paths():
     return [
         ('~/Downloads/data.zip', 'zip'),
         ('~/data.rar', 'rar'),
@@ -22,13 +25,43 @@ def paths():
         ('./../', '')
     ]
 
+@pytest.fixture
+def df_read_paths():
+    return [
+        ('tests/test_data/test.csv',  1218),
+        ('tests/test_data/test.xlsx', 1149),
+        ('tests/test_data/test.pkl',   936)
+    ]
+
+@pytest.fixture
+def df_read_exceptions():
+    return [
+        ('tests/test_data/test',    exceptions.PathError),
+        ('tests/context.py',        exceptions.UnknownFileTypeError),
+        ('tests/test_data/int.pkl', exceptions.DataFrameError)
+    ]
+
 @pytest.mark.io
-def test_get_extension(paths):
-    for path in paths:
+def test_get_extension(extension_paths):
+    for path in extension_paths:
         assert io.get_extension(path[0]) == path[1]
 
 @pytest.mark.io
 def test_get_extension_exception():
-    with pytest.raises(exceptions.PathError) as path_err:
+    with pytest.raises(TypeError) as _:
         io.get_extension(1)
     assert isinstance(io.get_extension('file.ext'), str)
+
+@pytest.mark.io
+def test_read_dataframe_success(df_read_paths):
+    for path, first_val in df_read_paths:
+        df = io.read_dataframe(path)
+        assert df.shape[0] == 30
+        assert df.PassengerId.iloc[0] == first_val
+
+@pytest.mark.io
+def test_read_dataframe_exceptions(df_read_exceptions):
+    for path, exception_obj in df_read_exceptions:
+        with pytest.raises(exception_obj) as _:
+            x = io.read_dataframe(path)
+            print('received:', x)
